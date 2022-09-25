821d06eb7ba48db7da54d24a59b7edd76c2a1a3f
xaax <85655796+xavitinho@users.noreply.github.com>
2022-09-25 14:53:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,18 @@
 const express = require('express')
 const app = express();
 
-const server = function() {
-  app.get('/', (request, response) => {
-    response.sendStatus(200);
-  })
-  app.listen(process.env.PORT);
-}
+app.use(express.static(__dirname));
 
-server()
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + '/widget.html');
+})
+
+app.listen(process.env.PORT);
 
 
 // twitter config
 
 const twit = require('twit')
-
 var T = new twit({
   consumer_key: process.env.CONSUMERTOKEN,
   consumer_secret: process.env.CONSUMERSECRET,
@@ -27,22 +25,20 @@ var T = new twit({
 })
 
 const { membros, termos } = require('./filter.json')
-
 const track = []
-
 for (let membro of membros) {
   for (let termo of termos) {
     track.push(`${membro} ${termo}`)
   }
 }
-
 console.log(track)
 
 var stream = T.stream('statuses/filter', { track })
-
 stream.on('tweet', function(tweet) {
   if (!tweet.retweeted_status) {
-    let text = tweet.extended_tweet ? tweet.extended_tweet.full_text : tweet.text
+    let text = tweet.extended_tweet 
+      ? tweet.extended_tweet.full_text.replace('https://', '') 
+      : tweet.text
     let skip = true
     for (let membro of membros) {
       for (let termo of termos) {
@@ -58,3 +54,22 @@ stream.on('tweet', function(tweet) {
     }
   }
 })
+
+const putzero = n => n.toLocaleString(undefined, { minimumIntegerDigits: 2 })
+
+
+// atualiza a bio
+
+setInterval( () => {
+  let t = new Date(Date.now())
+  t.setHours(t.getHours() - 3)
+  let h  = putzero(t.getHours())
+  let m  = putzero(t.getMinutes())
+  let d  = putzero(t.getDate())
+  let me = putzero(t.getMonth())
+  T.post('account/update_profile', { 
+    description: 
+      `ativa as notificações se estiver em busca de um beomgyu R ou outro photocard raro\n\nonline ${d}/${me} às ${h}h${m}m` 
+  })
+  console.log(`online ${d}/${me} às ${h}h${m}m`)
+}, 600000)
